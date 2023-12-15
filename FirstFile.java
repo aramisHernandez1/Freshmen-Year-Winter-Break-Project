ef6094e8316a3345844831b5cf20a737613a03de
aramisHernandez1 <ah1027406@wcupa.edu>
2023-12-15 18:48:06 -0500

    Update FirstFile.java

--- FirstFile.java
+++ FirstFile.java
@@ -1,3 +1,13 @@
-public FirstFile{
-  
+import java.util.Scanner;
+
+public class FirstFile {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter input to start:");
+        String read = input.nextLine();
+
+
+        input.close();
+    }
 }
