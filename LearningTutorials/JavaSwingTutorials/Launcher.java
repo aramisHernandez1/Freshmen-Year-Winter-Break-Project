e97a9b539da108ce4656fb15780e8345ba833da0
aramisHernandez1 <ah1027406@wcupa.edu>
2023-12-23 18:48:03 -0500

    Swing Tutorials

--- LearningTutorials/JavaSwingTutorials/Launcher.java
+++ LearningTutorials/JavaSwingTutorials/Launcher.java
@@ -9,7 +9,18 @@ public class Launcher {
     // All swing code has to run in the same thread. called event dispatch thread
     //Swing isnt thread safe.
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(null);//Will ensure its all ran on the event dispatch thread.
+        //Needs a Runnable instance
+        SwingUtilities.invokeLater(new Runnable() {
+
+            //Overide run method only method Runnable has.
+            @Override
+            public void run(){
+                //Create instance of MainWindow class.
+                MainWindow main = new MainWindow();
+                main.show();
+            }
+            
+        });//Will ensure its all ran on the event dispatch thread.
     }
 
 }
--- LearningTutorials/JavaSwingTutorials/MainWindow.java
+++ LearningTutorials/JavaSwingTutorials/MainWindow.java
@@ -1,7 +1,25 @@
 package LearningTutorials.JavaSwingTutorials;
 
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 
 public class MainWindow {
     
+    //JFrame = Top level container/window in Java Swing.
+    private JFrame window;
+
+    //Consturctor
+    public MainWindow(){
+        //Instance of JFrame
+        window = new JFrame();
+        window.setTitle("Hello World!");// Top name of window
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Makes it so when we click the top left X it actually closes the window instead of minimizing.
+        window.setSize(800, 500);//800 x 500 pixels
+        window.setLocationRelativeTo(null); //setting relative to null makes it cenentered on the screen.
+    
+    }
+
+    public void show(){
+        window.setVisible(true);
+    }
+
 }
