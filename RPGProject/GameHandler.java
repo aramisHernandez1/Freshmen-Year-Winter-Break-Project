9f24aa6e3c2f7cfae8f081557bc036092f466d92
aramisHernandez1 <ah1027406@wcupa.edu>
2023-12-29 18:36:52 -0500

    No longer using Java Swing
    Looking for a new Api for 2D Text based game

--- RPGProject/GameHandler.java
+++ RPGProject/GameHandler.java
@@ -5,11 +5,6 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-import RPGProject.GUI.BaseJFrame;
-import RPGProject.GUI.BasePanel;
-import RPGProject.GUI.ListButtons;
-import RPGProject.GUI.MainFrame;
-
 enum GameModes{
     PAUSED, PLAY
 }
@@ -19,7 +14,6 @@ public class GameHandler {
 
     private GameModes playMode;
 
-    private MainFrame frame;
 
     public GameHandler(){
         initialize();
@@ -27,7 +21,7 @@ public class GameHandler {
 
     private void initialize(){
         this.playMode = GameModes.PAUSED;
-        this.frame = new MainFrame();
+
 
 
     }
