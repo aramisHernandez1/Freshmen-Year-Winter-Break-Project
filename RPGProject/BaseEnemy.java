5a498b2cf59b3c5429663652288a331d0176fe55
aramisHernandez1 <ah1027406@wcupa.edu>
2024-01-03 17:56:43 -0500

    Update BaseEnemy.java

--- RPGProject/BaseEnemy.java
+++ RPGProject/BaseEnemy.java
@@ -1,4 +1,18 @@
 package RPGProject;
 public class BaseEnemy {
-    
+
+    private int maxHealth = 30;
+    private int power = 2;
+    public int health = maxHealth;
+    private int attackPower = 2 * power;
+
+
+    public void takeDamage(int damage){
+        this.health -= damage;
+    }
+
+    public int getAttackPower(){
+        return this.attackPower;
+    }
+
 }
